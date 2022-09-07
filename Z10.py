b0c2fe524e7d1c37243678ceb901f579ab2ee7a1
marmeladka63 <peshkova-va@yandex.ru>
2022-09-07 10:48:21 +0300

    ДЗ_2

--- Z10.py
+++ Z10.py
@@ -1,16 +1,16 @@
 #Реализуйте алгоритм перемешивания списка.
 import random
-spis=[1,2,19,12,"Мир"]
+spis=[1,2,19,12,"мир"]
 
 
 def mix_spis (spis):
 
-    for i in range(len(spis)):
-        index_random=random.randint(0,(len(spis)-1))
-        temp=spis[i]
-        spis[i]=spis[index_random]
-        spis[index_random]=temp
-        return spis
+    for i in range(len(spis)): #функция пермешивания списка
+        index_random=random.randint(0,(len(spis)-1)) # гененрируем случайные числа от 0 до 5
+        temp=spis[i] #записываем в перменную temp начальный список 
+        spis[i]=spis[index_random] #пишем в начальный список новые перемешанные данные
+        spis[index_random]=temp #присваеваем его перменной temp
+    return spis
 print("Исходный список: ")       
 print (spis)
 print("Перемешанный список: ")
